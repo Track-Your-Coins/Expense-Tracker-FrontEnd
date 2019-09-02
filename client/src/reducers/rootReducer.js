d1cfec063da68c96298de097bf5c5d920ff808c5
LadyKerr <kadiekerr@gmail.com>
2019-09-02 09:02:08 -0400

    rootReducer setUp

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,1 +1,10 @@
-//combine reducers here
+import { combineReducers } from "redux";
+import { loginReducer } from "./loginReducer";
+import { expenseReducer } from "./expenseReducer";
+import { incomeReducer } from "./incomeReducer";
+
+export default combineReducers({
+  login: loginReducer,
+  expense: expenseReducer,
+  income: incomeReducer
+});
