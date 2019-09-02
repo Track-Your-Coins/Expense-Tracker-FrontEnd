326fdd18584515aebf007402c3ba292748ded431
LadyKerr <kadiekerr@gmail.com>
2019-09-02 15:16:18 -0400

    axiosWithAuth

---
+++ client/src/utils/axiosWithAuth.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return axios.create({
+    headers: {
+      Authorization: token
+    },
+    baseURL: ""
+  });
+};
+
+//add baseURL once backend is deployed to heroku
