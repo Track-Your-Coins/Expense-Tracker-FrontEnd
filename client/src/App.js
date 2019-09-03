694cb7ef882358f47125b1a439a4acff739c527b
LadyKerr <kadiekerr@gmail.com>
2019-09-02 22:31:44 -0400

    updated login route in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navbar/NavBar";
+import LoginForm from "./components/navbar/forms/Login";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <h1>Track Your Coins in App</h1>
+      <Swicth>
+        <Route path="/login" component={LoginForm} />
+      </Swicth>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
