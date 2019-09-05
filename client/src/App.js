685bea375d92343c4c7247be580025732a1d27c5
LadyKerr <kadiekerr@gmail.com>
2019-09-05 09:08:20 -0400

    installed material ui and created footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import { Switch, Route, withRouter } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navbar/NavBar";
 import LoginForm from "./components/navbar/forms/Login";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <h1>Track Your Coins in App</h1>
+      <Footer />
       <Switch>
         <Route path="/login" component={LoginForm} />
+        {/* <Route path="/register" component={Register} /> */}
+        {/* <Route path="/dashboard" component={Dashboard} /> */}
       </Switch>
     </div>
   );
