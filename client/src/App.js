3ed2eca637318857ae3f6a23ca511a1736e8de09
LadyKerr <kadiekerr@gmail.com>
2019-09-05 09:19:33 -0400

    created loggedInNavBar component not rendered yet

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route, withRouter } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navbar/NavBar";
 import LoginForm from "./components/navbar/forms/Login";
+import Register from "./components/navbar/forms/Register";
+import Dashboard from "./components/home/dashboard/Dashboard";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -13,8 +15,8 @@ function App() {
       <Footer />
       <Switch>
         <Route path="/login" component={LoginForm} />
-        {/* <Route path="/register" component={Register} /> */}
-        {/* <Route path="/dashboard" component={Dashboard} /> */}
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </div>
   );
