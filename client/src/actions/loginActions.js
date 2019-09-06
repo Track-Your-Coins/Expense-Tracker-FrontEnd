f827f61ce1056bec6188a392910a80705108cbe3
LadyKerr <kadiekerr@gmail.com>
2019-09-05 22:50:40 -0400

    signup actions

--- client/src/actions/loginActions.js
+++ client/src/actions/loginActions.js
@@ -9,11 +9,23 @@ export const login = creds => dispatch => {
   return axiosWithAuth();
 };
 
+//new user registration
 export const SIGNUP_START = "SIGNUP_START";
 export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
 export const SIGNUP_FAILURE = "SIGNUP_FAILURE";
 
 export const signUp = newUser => dispatch => {
-  dispatch({});
-  return axiosWithAuth();
+  dispatch({ type: SIGNUP_START });
+  return axiosWithAuth()
+    .post("/api/auth/register", newUser)
+    .then(res => {
+      console.log("signUp Action", res);
+      localStorage.setItem("token", red.data.token);
+      dispatch({ type: SIGNUP_SUCCESS, payload: res.data });
+      return true;
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: SIGNUP_FAILURE, payload: err });
+    });
 };
