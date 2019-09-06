6bde647f1ff0881173c58c70dbd6fccbccc2c567
LadyKerr <kadiekerr@gmail.com>
2019-09-05 22:54:03 -0400

    signup reducers

--- client/src/reducers/loginReducer.js
+++ client/src/reducers/loginReducer.js
@@ -7,12 +7,31 @@ import {
   SIGNUP_FAILURE
 } from "../actions/loginActions";
 
-const initialState = {};
+const initialState = {
+  addingUsers: false,
+  error: ""
+};
 
 export const loginReducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOGIN_START:
-      return {};
+    case SIGNUP_START:
+      return {
+        ...state,
+        addingUsers: true,
+        error: ""
+      };
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        addingUsers: false,
+        error: ""
+      };
+    case SIGNUP_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        addingUsers: false
+      };
     default:
       return state;
   }
