42ab805a36ab4f9bea7e565fa1acc87e0990fe54
LadyKerr <kadiekerr@gmail.com>
2019-09-05 23:59:50 -0400

    login reducer

--- client/src/actions/loginActions.js
+++ client/src/actions/loginActions.js
@@ -5,9 +5,9 @@ export const SIGNUP_START = "SIGNUP_START";
 export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
 export const SIGNUP_FAILURE = "SIGNUP_FAILURE";
 
-export const signUp = (history, newUser) => dispatch => {
+export const signUp = newUser => dispatch => {
   dispatch({ type: SIGNUP_START });
-  axiosWithAuth()
+  return axiosWithAuth()
     .post(
       "https://track-your-coins-be.herokuapp.com/api/auth/register",
       newUser
@@ -16,6 +16,7 @@ export const signUp = (history, newUser) => dispatch => {
       console.log("signUp Action", res);
       localStorage.setItem("token", res.data.token);
       dispatch({ type: SIGNUP_SUCCESS, payload: res.data });
+      return true;
     })
     .catch(err => {
       console.log(err);
@@ -23,11 +24,22 @@ export const signUp = (history, newUser) => dispatch => {
     });
 };
 
+//registered user login
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
 export const login = creds => dispatch => {
-  dispatch({});
-  return axiosWithAuth();
+  dispatch({ type: LOGIN_START });
+  return axiosWithAuth()
+    .post("https://track-your-coins-be.herokuapp.com/api/auth/login", creds)
+    .then(res => {
+      console.log("loginRes", res);
+      localStorage.setItem("token", res.data.token);
+      dispatch({ type: LOGIN_SUCCESS });
+      return true;
+    })
+    .catch(err => {
+      dispatch({ type: LOGIN_FAILURE, payload: err });
+    });
 };
