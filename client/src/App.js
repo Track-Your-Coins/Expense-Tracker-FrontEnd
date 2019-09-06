0e64e759c9858a05bb7944ac7564c54383c882cc
LadyKerr <kadiekerr@gmail.com>
2019-09-06 00:55:56 -0400

    conditionally rendered navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/navbar/NavBar";
+import LoggedInNavBar from "./components/navbar/LoggedInNavBar";
 import LoginForm from "./components/navbar/forms/Login";
 import Register from "./components/navbar/forms/Register";
 import Dashboard from "./components/home/dashboard/Dashboard";
 import Footer from "./components/footer/Footer";
 
 function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <div className="App">
-      <NavBar />
+      {token ? <LoggedInNavBar /> : <NavBar />}
       <div id="app-container">
         <Switch>
           <Route path="/login" component={LoginForm} />
