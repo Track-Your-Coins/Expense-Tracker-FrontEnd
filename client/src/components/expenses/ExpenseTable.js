bf5fea6205ac37dcf1ae93a60e473a1b87f78784
LadyKerr <kadiekerr@gmail.com>
2019-09-14 00:45:19 -0400

    pulling user expenses

--- client/src/components/expenses/ExpenseTable.js
+++ client/src/components/expenses/ExpenseTable.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { withRouter } from "react-router";
 import { connect } from "react-redux";
 import { fetchExpense } from "../../actions/expenseActions";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Button from "@material-ui/core/Button";
 
 const ExpenseTable = props => {
   console.log("Expense Table Props:", props);
-  return <div>Hello from Expenses</div>;
+  const { expenses } = props;
+
+  useEffect(() => {
+    const id = localStorage.getItem("user_id");
+    props.fetchExpense(id);
+  });
+
+  return (
+    <div>
+      <h6>Expenses</h6>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Category</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>Notes</TableCell>
+            <TableCell>Outstanding</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {expenses.map(expense => (
+            <TableRow key={expense.id}>
+              <TableCell>{expense.date}</TableCell>
+              <TableCell>{expense.category}</TableCell>
+              <TableCell>{expense.amount}</TableCell>
+              <TableCell>{expense.notes}</TableCell>
+              <TableCell>{expense.paid}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 const mapStateToProps = state => ({
